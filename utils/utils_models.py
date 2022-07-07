8bcb6304d6eb3a196a9bf22a4d01cf14570804f6
Rasheed19 <ibraheem.abdulrasheed@gmail.com>
2022-07-07 11:49:30 +0100

    modify plot function

--- utils/utils_models.py
+++ utils/utils_models.py
@@ -2,7 +2,9 @@ import os
 import pickle
 import pandas as pd
 from scipy.optimize import curve_fit
+from scipy import stats
 from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.preprocessing import StandardScaler
 from xgboost import XGBRegressor
 import xgboost as xgb
 from sklearn.svm import NuSVR
@@ -74,7 +76,7 @@ def metrics_calculator(y_true, y_pred):
             MAE, MAPE, MSE, RMSE, and R2 score.
     '''
     return  {'MAE': mean_absolute_error(y_true, y_pred), 'MAPE': mean_absolute_percentage_error(y_true, y_pred), 'MSE': mean_squared_error(y_true, y_pred),
-               'RMSE': np.sqrt(mean_squared_error(y_true, y_pred)), 'R2 score': r2_score(y_true, y_pred)}
+               'RMSE': np.sqrt(mean_squared_error(y_true, y_pred)), 'R2 score': r2_score(y_true, y_pred), 'Corr': stats.pearsonr(y_true, y_pred)[0]}
 
 
 def plot_prediction_experimental(y_train_true, y_train_pred, y_test_true, y_test_pred, fname, title=None, plot_mode=1):
@@ -142,9 +144,9 @@ def plot_prediction_experimental(y_train_true, y_train_pred, y_test_true, y_test
             res = pair[0]-pair[1]
             x_min = min(res)
             x_max = max(res)
-            subaxis.hist(res, bins=8, range=(x_min, x_max), density=True, color='green', ec='black')
+            subaxis.hist(res, bins=15, range=(x_min, x_max), color='green', ec='black')
             subaxis.set_xlabel('Residual', fontsize=12)
-            subaxis.set_ylabel('Density', fontsize=12)
+            subaxis.set_ylabel('Frequency', fontsize=12)
     
     elif plot_mode == 2:
 
@@ -171,10 +173,40 @@ def plot_prediction_experimental(y_train_true, y_train_pred, y_test_true, y_test
         subaxis.hist(res, bins=8, range=(x_min, x_max), color='purple', ec='black')
         subaxis.set_xlabel('Residual', fontsize=12)
         subaxis.set_ylabel('Frequency', fontsize=12)
+    
+    elif plot_mode == 3:
 
+        fig, axes = plt.subplots(1, 2, figsize=(15,10))
+
+        for axis, s, pair in zip(axes.ravel(), ('(train)', '(test)'), ((y_train_true, y_train_pred), (y_test_true, y_test_pred))):
+            
+            axis.scatter(pair[0], pair[1], s=100, color='purple', alpha=0.5, zorder=10)
+            lims = [
+            np.min([axis.get_xlim(), axis.get_ylim()]),  # min of both axes
+            np.max([axis.get_xlim(), axis.get_ylim()]),  # max of both axes
+            ]
+            # now plot both limits against each other
+            axis.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
+            axis.set_aspect('equal')
+            axis.set_xlim(lims)
+            axis.set_ylim(lims)
+            axis.set_xlabel('Experimental EOL ' + s, fontsize=14)
+            axis.set_ylabel('Predicted EOL ' + s, fontsize=14)
+
+            subaxis = add_sub_axes(axis, [0.6, 0.3, 0.3, 0.2])
+            res = pair[0]-pair[1]
+            x_min = min(res)
+            x_max = max(res)
+            subaxis.hist(res, bins=15, range=(x_min, x_max), color='purple', ec='black')
+            subaxis.set_xlabel('Residual', fontsize=12)
+            subaxis.set_ylabel('Frequency', fontsize=12)
+        
+        fig = axes[0].get_figure()
+        fig.tight_layout()
+        fig.subplots_adjust(top=1.2)
+        plt.suptitle(title, fontsize=18)
 
     plt.savefig(fname="plots/"+"pred_vs_true_"+fname, bbox_inches='tight')
-    plt.show()
 
 def repeated_kfold_cross_validation(model, df, n_splits, n_repeats, feature_selection, scaling, k=None):
     '''
@@ -197,7 +229,7 @@ def repeated_kfold_cross_validation(model, df, n_splits, n_repeats, feature_sele
 
     # feature selection option
     if feature_selection == True:
-        X, _, _ = utils_gn.univariate_feature_selection(df, k=k)
+        _, X, _, _ = utils_gn.univariate_feature_selection(df, k=k)
     
     # scaling option
     if scaling == True:
@@ -216,14 +248,12 @@ def repeated_kfold_cross_validation(model, df, n_splits, n_repeats, feature_sele
     return scores
 
 
-def fit_tree_based_regression(df, test_size, feature_selection, scaling, params, plot, fname, title=None, model_type='xgb', k=None):
+def fit_tree_based_regression(df, scaling, params, plot, fname, title=None, model_type='xgb', verbose=0):
     '''
     A function that fits XGBoost Regression to data.
 
     Arguments:
               df:                 pandas dataframe that contains the features and the target
-              test_size:          percentage of data to be used for testing
-              feature_selection:  a boolean to specify whether to perfrom feature selection or not
               scaling:            a boolean to specify whether to perform data scaling or not
               params:             a dictionary of parameters to pass to the regression object
               plot:               a boolean to specify whether to plot feature importance
@@ -231,6 +261,7 @@ def fit_tree_based_regression(df, test_size, feature_selection, scaling, params,
               title:              plot title
               model_type:         either to fit XGBoost ('xgb') or Extratrees ('ext') regression
               k:                  fraction of features to select if feature selection is set to true
+              verbose:            controls the amout of infomation printed on the screen during training
     
     Returns:
             model object and print dictionary of metrics(mae, mape, mse, rmse, r2)
@@ -238,19 +269,17 @@ def fit_tree_based_regression(df, test_size, feature_selection, scaling, params,
     '''
 
     # get the features and the target 
-    X, y = df.drop(df.columns[-1], axis=1).values, df[df.columns[-1]].values
+    label = df['train'].columns[-1]
+    X_train, y_train = df['train'].drop(label, axis=1).values, df['train'][label].values
+    X_test, y_test = df['test'].drop(label, axis=1).values, df['test'][label].values
 
-    # feature selection option
-    if feature_selection == True:
-        X, _, _ = utils_gn.univariate_feature_selection(df, k=k)
-    
     # scaling option
     if scaling == True:
-        X = utils_gn.scaler(X)
+        sc = StandardScaler()
+        sc = sc.fit(X_train)
+        X_train = sc.transform(X_train)
+        X_test = sc.transform(X_test)
     
-    # perform test-train split 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
-
     # fit the model and get the feature importances 
     print("Tree-based regression has started...") 
     start_time = time.time()
@@ -272,18 +301,20 @@ def fit_tree_based_regression(df, test_size, feature_selection, scaling, params,
     
     # calculate metrics 
     metrics = metrics_calculator(y_train, y_pred_train), metrics_calculator(y_test, y_pred_test)
-    print('------------------')
-    print('Model metrics:')
-    print('------------------')
-    print('Train:')
-    pprint.pprint(metrics[0])
-    print('Test:')
-    pprint.pprint(metrics[1])
-    
+
+    if verbose > 0:
+        print('------------------')
+        print('Model metrics:')
+        print('------------------')
+        print('Train:')
+        pprint.pprint(metrics[0])
+        print('Test:')
+        pprint.pprint(metrics[1])
+        
     
     # option to plot feature importance, plot the first 30 most important features
     if plot == True:
-        features, feature_importance = utils_gn.feature_importance_ordering(df.columns[:-1], model.feature_importances_)
+        features, feature_importance = utils_gn.feature_importance_ordering(df['train'].columns[:-1], model.feature_importances_)
         utils_gn.feature_importance_barchart(features[:30], feature_importance[:30], 'Feature importance', fname)
         plot_prediction_experimental(y_train, y_pred_train, y_test, y_pred_test, fname, title)
     
@@ -294,20 +325,18 @@ def fit_tree_based_regression(df, test_size, feature_selection, scaling, params,
     return model, metrics
 
 
-def fit_nusvr(df, test_size, feature_selection, scaling, params, fname, title=None, plot=False, k=None):
+def fit_nusvr(df, scaling, params, fname, title=None, plot=False, verbose=0):
     '''
     A function that fits XGBoost Regression to data.
 
     Arguments:
               df:                 pandas dataframe that contains the features and the target
-              test_size:          percentage of data to be used for testing
-              feature_selection:  a boolean to specify whether to perfrom feature selection or not
               scaling:            a boolean to specify whether to perform data scaling or not
               params:             a dictionary of parameters to pass to the regression object
               plot:               a boolean to specify whether to plot feature importance
               fname:              name to save the model
               title:              plot title
-              k:                  fraction of features to select if feature selection is set to true
+              verbose:            controls the amout of infomation printed on the screen during training
     
     Returns:
             model object and print dictionary of metrics(mae, mape, mse, rmse, r2)
@@ -315,18 +344,16 @@ def fit_nusvr(df, test_size, feature_selection, scaling, params, fname, title=No
     '''
 
     # get the features and the target 
-    X, y = df.drop(df.columns[-1], axis=1).values, df[df.columns[-1]].values
-
-    # feature selection option
-    if feature_selection == True:
-        X, _, _ = utils_gn.univariate_feature_selection(df, k=k)
+    label = df['train'].columns[-1]
+    X_train, y_train = df['train'].drop(label, axis=1).values, df['train'][label].values
+    X_test, y_test = df['test'].drop(label, axis=1).values, df['test'][label].values
     
     # scaling option
     if scaling == True:
-        X = utils_gn.scaler(X)
-    
-    # perform test-train split 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        sc = StandardScaler()
+        sc = sc.fit(X_train)
+        X_train = sc.transform(X_train)
+        X_test = sc.transform(X_test)
 
     # fit the model and get the feature importances 
     print("NuSVR training has started...") 
@@ -344,16 +371,18 @@ def fit_nusvr(df, test_size, feature_selection, scaling, params, fname, title=No
     
    # calculate metrics 
     metrics = metrics_calculator(y_train, y_pred_train), metrics_calculator(y_test, y_pred_test)
-    print('------------------')
-    print('Model metrics:')
-    print('------------------')
-    print('Train:')
-    pprint.pprint(metrics[0])
-    print('Test:')
-    pprint.pprint(metrics[1])
+
+    if verbose > 0:
+        print('------------------')
+        print('Model metrics:')
+        print('------------------')
+        print('Train:')
+        pprint.pprint(metrics[0])
+        print('Test:')
+        pprint.pprint(metrics[1])
     
     if params['kernel']=='linear' and plot==True:
-        features, feature_importance = utils_gn.feature_importance_ordering(df.columns[:-1], np.abs(np.ravel(model.coef_)))
+        features, feature_importance = utils_gn.feature_importance_ordering(df['train'].columns[:-1], np.abs(np.ravel(model.coef_)))
         utils_gn.feature_importance_barchart(features[:30], feature_importance[:30], 'Feature weight', fname)
 
     # option to plot feature importance, plot the first 30 most important features
@@ -366,7 +395,7 @@ def fit_nusvr(df, test_size, feature_selection, scaling, params, fname, title=No
     
     return model, metrics
 
-def hyperparameter_tuning(df, estimator, param_grid, scoring, cv, feature_selection=False, k=None):
+def hyperparameter_tuning(df, estimator, param_grid, scoring, cv, scaling=True):
     '''
     A function that performs grid search over the space of parameters given.
 
@@ -381,13 +410,16 @@ def hyperparameter_tuning(df, estimator, param_grid, scoring, cv, feature_select
 
     Returns: best parameter setting, best score
     '''
-
     # get the features and the target 
-    X, y = df.drop(df.columns[-1], axis=1).values, df[df.columns[-1]].values
+    label = df['validate'].columns[-1]
+    X, y = df['validate'].drop(label, axis=1).values, df['validate'][label].values
+    
+    # scaling option
+    if scaling == True:
+        sc = StandardScaler()
+        sc = sc.fit(df['train'].drop(label, axis=1).values)
+        X = sc.transform(X)
 
-    # feature selection option
-    if feature_selection == True:
-        X, _, _ = utils_gn.univariate_feature_selection(df, k=k)
     
     # define the GridSearchCV object and fit to the data 
     gs = GridSearchCV(estimator=estimator, param_grid=param_grid, scoring=scoring, cv=cv).fit(X, y)
@@ -396,66 +428,80 @@ def hyperparameter_tuning(df, estimator, param_grid, scoring, cv, feature_select
 
 
 
-def model_pipeline(df, algo, estimator, param_grid, fname, title=None, test_size=0.2, scoring='neg_mean_absolute_percentage_error', cv=3, model_type=None):
+def model_pipeline(df,
+                algo,
+                estimator,
+                param_grid,
+                fname,
+                feature_selection=True,
+                val_ratio=0.2,
+                test_ratio=0.2,
+                title=None,
+                scoring='neg_mean_absolute_percentage_error',
+                cv=3, 
+                model_type=None,
+                scaling=None,
+                k_list = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5]
+                ):
     '''
     This function implements pipeline for optimal hyper-parameter searching, feature selection and model building.
 
     Args:
-         df:           data for modelling
-         algo:         type of algorithm for modelling
-         estimator:    model object
-         param_grid:   space of parameters
-         fname:        string to save the results with
+         df:                 data for modelling
+         algo:               type of algorithm for modelling
+         estimator:          model object
+         param_grid:         space of parameters
+         fname:              string to save the results with
+         feature_selection:  a boolean either to perform feature selection or not
+         val_ratio:          ratio of validation set
+         test_ratio:         ratio of test set
          title:              plot title
-         test_size:    test set size
-         scoring:      scoring function
-         cv:           cross-validation fold size
-         model_type:   type of model (to be used with tree-based model
+         scoring:            scoring function
+         cv:                 cross-validation fold size
+         model_type:         type of model (to be used with tree-based model
+         scaling:            a boolean either to scale features or not
+         k_list:             a list containing fractions of features to be extracted from features selection process
     
     Returns: best feature selection strategy (in terms of proportion), corresponding parameters, and dataframe of metrics
     '''
     dict_of_opt_params = {}
     metric_list = []
 
-    # create a list of k
-    k_list = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]
-
     for k in k_list:
 
+        # generate balanced train, validation and test data
+        bl_df = utils_gn.balance_batches(df,
+                                        val_ratio,
+                                        test_ratio,
+                                        feature_selection,
+                                        k)
+
         # search for the best hyper-parameters
-        best_param, _ = hyperparameter_tuning(df=df,
+        best_param, _ = hyperparameter_tuning(df=bl_df,
                                             estimator=estimator,
                                             param_grid=param_grid,
                                             scoring=scoring,
-                                            cv=cv,
-                                            feature_selection=True,
-                                            k=k)
+                                            cv=cv)
 
         # store the best parameters in the dictionary
         dict_of_opt_params[k] = best_param
 
         # use the best parameters to build model 
         if model_type is None:
-            model, metrics = algo(df=df,
-                                test_size=test_size,
-                                feature_selection=True,
-                                scaling=False,
+            model, metrics = algo(df=bl_df,
+                                scaling=scaling,
                                 params=best_param,
                                 plot=True,
                                 title=title,
-                                fname=fname+str(int(k*100)),
-                                k=k)
+                                fname=fname+str(int(k*100)))
         else:
-            model, metrics = algo(df=df,
-                                test_size=test_size,
-                                feature_selection=True,
-                                scaling=False,
+            model, metrics = algo(df=bl_df,
+                                scaling=scaling,
                                 params=best_param,
                                 plot=True,
                                 title=title,
                                 fname=fname+str(int(k*100)),
-                                model_type=model_type,
-                                k=k)
+                                model_type=model_type)
         
         metric_list.append(list(metrics[0].values()) + list(metrics[1].values()))
 
